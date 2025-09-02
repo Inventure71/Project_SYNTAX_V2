fc16f106a9a0b7ce29ae1042a82ac2d2ec372eec
inventure71 <matteo.giorgetti.05@gmail.com>
2025-09-02 17:12:09 +0200

    Refactor restore_backup call in test.py to remove unnecessary file_path argument. Update compiled handle_backup.pyc file.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from Agent.Helpers.handle_backup import save_backup, restore_backup, delete_backup
 
 #save_backup()
-restore_backup(file_path="Test", auto_delete=True)
+restore_backup(auto_delete=True)
 #delete_backup()
